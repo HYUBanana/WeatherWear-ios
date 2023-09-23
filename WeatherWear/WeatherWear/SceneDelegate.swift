664f7ac254c3dbde6ec680d2c7ffc5a21700ebe0
LURKS02 <thecrooks2@gmail.com>
2023-09-23 17:23:43 +0900

    #9-(feature): implement service provider

--- WeatherWear/WeatherWear/SceneDelegate.swift
+++ WeatherWear/WeatherWear/SceneDelegate.swift
@@ -15,7 +15,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        let mainViewController = SettingViewController()
+        let serviceProvider: ServiceProviderType = MockServiceProvider()
+        let mainViewController = SettingViewController(serviceProvider: serviceProvider)
 
         window = UIWindow(windowScene: windowScene)
         window?.rootViewController = mainViewController
---
+++ WeatherWear/WeatherWear/Services/AutoOptionService.swift
@@ -0,0 +1,18 @@
+//
+//  AutoSettingService.swift
+//  WeatherWear
+//
+//  Created by 디해 on 2023/09/23.
+//
+
+protocol AutoOptionServiceType {
+    
+}
+
+final class AutoOptionService: AutoOptionServiceType {
+    
+}
+
+final class MockAutoOptionService: AutoOptionServiceType {
+    
+}
